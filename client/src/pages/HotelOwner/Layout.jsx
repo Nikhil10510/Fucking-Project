3f109705c4e52e313b316d29573535270884d885
Nikhil10510 <nikhilkumarr101@gmail.com>
2025-07-22 13:48:36 +0530

    fix: correct casing in import paths for Navbar and Sidebar components

--- client/src/pages/HotelOwner/Layout.jsx
+++ client/src/pages/HotelOwner/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from '../../components/HotelOwner/Navbar'
-import Sidebar from '../../components/HotelOwner/SideBar'
+import Navbar from '../../components/hotelOwner/Navbar'
+import Sidebar from '../../components/hotelOwner/SideBar'
 import { Outlet } from 'react-router-dom'
 import { useAppContext } from '../../context/AppContext'
 import { useEffect } from 'react'
